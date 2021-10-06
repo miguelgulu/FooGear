f2cf7a491ff4839551a1422125ee0754775a34db
miguelgulu <miguel.guerrero.luna.alu@iesfernandoaguilar.es>
2021-10-06 18:55:16 +0200

    Commit 2

--- FooGear/settings.py
+++ FooGear/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'FooGearApp.apps.FoogearappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- FooGearApp/models.py
+++ FooGearApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model): 
+	idstock = models.IntegerField()
+
+class Producto(models.Model):
+	choice_tipo = [('C', 'Camiseta'), ('M', 'Medias'), ('CL','Calzonas'), ('S','Sudaderas')]
+	choice_talla = [('XS','XS'), ('S','S'), ('M','M'), ('L','L'), ('XL','XL')]
+	idstock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+	tipo = models.CharField(max_length=100, choices=choice_tipo) #Choice
+	talla = models.CharField(max_length=3, choices=choice_talla) #Choice
+	cantidad = models.IntegerField()
+	precio = models.IntegerField()
+
+class Comprador(models.Model):
+	nombre=models.CharField(max_length=100)
+	telefono=models.IntegerField()
+	direccion=models.CharField(max_length=150)
+
+class Reserva(models.Model):
+	producto=models.ManyToManyField(Producto)
+	comprador=models.ForeignKey(Comprador, on_delete=models.CASCADE)
+	idReserva=models.IntegerField()
